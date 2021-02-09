e8f10dc4f2f725805f2134e817bf2db633d8e287
thirtytech <thirtytech@outlook.com>
2021-02-08 23:10:31 -0100

    added Parameters

---
+++ MagmaSynthesizer_LAVA/MagmaSynthesiser_LAVA/Source/Parameters.h
@@ -0,0 +1,41 @@
+/*
+  ==============================================================================
+
+    Parameters.h
+    Created: 8 Feb 2021 10:52:17pm
+    Author:  thirt
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#define GAIN_ID = "GAIN_ID"
+#define GAIN_LABEL = "Gain"
+
+#define OSC1_ID = "OSC1_ID"
+#define OSC1_LABEL = "Osc 1"
+
+#define OSC2_ID = "OSC2_ID"
+#define OSC2_LABEL = "Osc 2"
+
+#define OSC3_ID = "OSC3_ID"
+#define OSC3_LABEL = "Osc 3"
+
+#define ADSR1_ID = "ADSR1_ID"
+#define ADSR1_LABEL = "ADSR 1"
+
+#define ADSR2_ID = "ADSR2_ID"
+#define ADSR2_LABEL = "ADSR 2"
+
+#define ADSR3_ID = "ADSR3_ID"
+#define ADSR3_LABEL = "ADSR 3"
+
+#define FILTER1_ID = "FILTER1_ID"
+#define FILTER1_LABEL = "Filter 1"
+
+#define FILTER2_ID = "FILTER2_ID"
+#define FILTER2_LABEL = "Filter 2"
+
+#define FILTER3_ID = "FILTER3_ID"
+#define FILTER3_LABEL = "Filter 3"
