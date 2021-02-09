fe491ada006745529504140420a29d00d1c7f0b6
thirtytech <thirtytech@outlook.com>
2021-02-08 23:30:12 -0100

    added ADSR sliders and slider attachments

--- MagmaSynthesizer_LAVA/MagmaSynthesiser_LAVA/Source/PluginEditor.cpp
+++ MagmaSynthesizer_LAVA/MagmaSynthesiser_LAVA/Source/PluginEditor.cpp
@@ -8,6 +8,7 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "Parameters.h"
 
 //==============================================================================
 MagmaSynthesiser_lavaAudioProcessorEditor::MagmaSynthesiser_lavaAudioProcessorEditor (MagmaSynthesiser_lavaAudioProcessor& p)
@@ -17,7 +18,22 @@ MagmaSynthesiser_lavaAudioProcessorEditor::MagmaSynthesiser_lavaAudioProcessorEd
     gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 50);
     addAndMakeVisible(gainSlider);
 
-    gainSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, "GAIN", gainSlider);
+    gainSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, GAIN_ID, gainSlider);
+
+    adsr1_attackSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR1_ATTACK_ID, adsr1_attackSlider);
+    adsr1_decaySliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR1_DECAY_ID, adsr1_decaySlider);
+    adsr1_sustainSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR1_SUSTAIN_ID, adsr1_sustainSlider);
+    adsr1_releaseSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR1_RELEASE_ID, adsr1_releaseSlider);
+
+    adsr2_attackSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR2_ATTACK_ID, adsr2_attackSlider);
+    adsr2_decaySliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR2_DECAY_ID, adsr2_decaySlider);
+    adsr2_sustainSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR2_SUSTAIN_ID, adsr2_sustainSlider);
+    adsr2_releaseSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR2_RELEASE_ID, adsr2_releaseSlider);
+
+    adsr3_attackSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR3_ATTACK_ID, adsr3_attackSlider);
+    adsr3_decaySliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR3_DECAY_ID, adsr3_decaySlider);
+    adsr3_sustainSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR3_SUSTAIN_ID, adsr3_sustainSlider);
+    adsr3_releaseSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, ADSR3_RELEASE_ID, adsr3_releaseSlider);
 
     setSize (400, 300);
 }
--- MagmaSynthesizer_LAVA/MagmaSynthesiser_LAVA/Source/PluginEditor.h
+++ MagmaSynthesizer_LAVA/MagmaSynthesiser_LAVA/Source/PluginEditor.h
@@ -26,8 +26,34 @@ public:
 
 private:
     juce::Slider gainSlider;
-
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainSliderAttachment;
+    
+    juce::Slider adsr1_attackSlider;
+    juce::Slider adsr1_decaySlider;
+    juce::Slider adsr1_sustainSlider;
+    juce::Slider adsr1_releaseSlider;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr1_attackSliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr1_decaySliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr1_sustainSliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr1_releaseSliderAttachment;
+
+    juce::Slider adsr2_attackSlider;
+    juce::Slider adsr2_decaySlider;
+    juce::Slider adsr2_sustainSlider;
+    juce::Slider adsr2_releaseSlider;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr2_attackSliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr2_decaySliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr2_sustainSliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr2_releaseSliderAttachment;
+
+    juce::Slider adsr3_attackSlider;
+    juce::Slider adsr3_decaySlider;
+    juce::Slider adsr3_sustainSlider;
+    juce::Slider adsr3_releaseSlider;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr3_attackSliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr3_decaySliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr3_sustainSliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsr3_releaseSliderAttachment;
 
     MagmaSynthesiser_lavaAudioProcessor& audioProcessor;
 
